12d5766a0339ee71fe13fe8c77c9044a2a940850
lexi-luo <lexi-luo@github.com>
2018-07-21 17:19:00 +1200

    panda of trash

--- minesweeper.js
+++ minesweeper.js
@@ -1,73 +1,51 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+
 // Define your `board` object here!
+/*var board = {
+  cells:[ 
+      {row:0,col:0,isMine:false,hidden:true},
+      {row:0,col:1,isMine:false,hidden:true},
+      {row:0,col:2,isMine:true,hidden:true},
+      {row:1,col:0,isMine:false,hidden:true},
+      {row:1,col:1,isMine:false,hidden:true},
+      {row:1,col:2,isMine:true,hidden:true},
+      {row:2,col:0,isMine:false,hidden:true},
+      {row:2,col:1,isMine:true,hidden:true},
+      {row:2,col:2,isMine:false,hidden:true}
+  ]
+}
+*/
+
 var board = {
-  cells:[ {row: 0,
-    col: 0,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
+  cells: []
+}
 
-    {row: 0,
-    col: 1,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
-
-    {row: 0,
-    col: 2,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
-
-    {row: 1,
-    col: 0,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
-
-    {row: 1,
-    col: 1,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
-
-    {row: 1,
-    col: 2,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
-
-    {row: 2,
-    col: 0,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
-
-    {row: 2,
-    col: 1,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    },
-
-    {row: 2,
-    col: 2,
-    isMine: Boolean(Math.floor(Math.random()*1.5)),
-    isMarked: false,
-    hidden: true,
-    } ]
+var boardSize = 6;
+  
+function gameBoard() {
+  for (var r = 0; r < boardSize; r++) {
+    for (var c = 0; c < boardSize; c++) {
+      board.cells.push({
+        row: r,
+        col: c,
+        isMine: Boolean(Math.floor(Math.random()*1.5)),
+        isMarked: false,
+        hidden: true,
+      })
+    }
+  }
 }
 
 function startGame () {
+  gameBoard();
   // Don't remove this function call: it makes the game work!
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+    document.addEventListener ('click', checkForWin)
+    document.addEventListener ('contextmenu', checkForWin)
+  }
+
   lib.initBoard()
 }
 
@@ -76,11 +54,16 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-
+  for (var i = 0; i < board.cells.length; i++) {
+    if (!board.cells[i].isMine && board.cells[i].hidden) {return}
+    if (board.cells[i].isMine && !board.cells[i].isMarked) {return}
+    }
+lib.displayMessage('no rabies for you!')
+  }
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
-}
+
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
